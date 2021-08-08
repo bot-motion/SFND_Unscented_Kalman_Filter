adb0b5834ee32a68264c3330d5ce46d286c4240d
Matthias Schmitt <botmotion@outlook.com>
2021-08-08 16:33:42 +0200

    Experimenting with Eigen vectors as parameters

---
+++ src/argument_check.cpp
@@ -0,0 +1,22 @@
+
+#include "Eigen/Dense"
+#include <iostream>
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+VectorXd testFn(VectorXd &x)
+{
+    x(0) = 5;
+    return x;
+}
+
+void main()
+{    
+	VectorXd x_pre;
+    x_pre << 1,  1, 1, 0, 0;
+
+    VectorXd x_post = testFn(x_pre);
+
+    std::cout << x_post;
+}
